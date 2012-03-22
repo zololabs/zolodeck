40093177190f0d19d64c303c7f98a8663e436cf8
Amit Rathore <amitrathore@gmail.com>
2012-03-21 18:02:52 -0700

    adding lein-clojure-mode etc

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
                  "local" ~(str (.toURI (java.io.File. 
                                  (str (System/getProperty "user.name") "/.zolo.mvn"))))}
   :dev-resources-path "script"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.4.0-beta3"]
+;                 [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.6.2"]
                  [ring/ring-core "0.3.7"]
                  [ring/ring-jetty-adapter "0.2.3"]
                  [sandbar/sandbar "0.3.0"]
                  [clj-base64 "0.0.1"]
                  [org.bituf/clj-stringtemplate "0.2"]
-                 [org.slf4j/slf4j-log4j12 "1.6.1"]
+                 [org.slf4j/slf4j-log4j12 "1.6.4"]
                  [clj-facebook-graph "0.2.0"]
                  [bouncycastle/bcprov-jdk16-nosign "140"]
                  [clj-http "0.1.3"]
@@ -20,6 +20,12 @@
 
                  [org.clojars.nakkaya.javax.mail/imap "1.4.3"]
                  [org.clojars.nakkaya.javax.mail/mail "1.4.3"]
+
+
+                 [org.codehaus.janino/janino "2.5.16"]
+                 [org.apache.lucene/lucene-core "3.3.0"]
+                 [org.hornetq/hornetq-core "2.2.2.Final"]
+                 [com.amazonaws/aws-java-sdk "1.3.0"]
 
                  [commons-io "1.4"]
                  [org.apache.commons/commons-exec "1.1"]
@@ -41,5 +47,6 @@
                  [datomic/peer "0.1.2678"]
 
 ]
+  :plugins [[lein-swank "1.4.3"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :resources-path "config")
--- src/zolo/config.clj
+++ src/zolo/config.clj
@@ -1,2 +1,43 @@
 (ns zolo.config)
 
+(declare CONFIG-MAP ENV)
+
+(defn load-config [config-file env]
+  (def ENV env)
+  (def CONFIG-MAP (load-string (slurp config-file))))
+
+(defn production-mode? []
+  (= :production ENV))
+
+(defn db-spec []
+  (get-in CONFIG-MAP [ENV :db-spec]))
+
+(defn mail-spec []
+  (get-in CONFIG-MAP [ENV :mail-spec]))
+
+(defn apns-password []
+  (get-in CONFIG-MAP [ENV :apns-password]))
+
+(defn apns-env []
+  (get-in CONFIG-MAP [ENV :apns-env]))
+
+(defn apns-production? []
+  (= :production (apns-env)))
+
+(defn yipit-key []
+  (get-in CONFIG-MAP [ENV :yipit-key]))
+
+(defn yipit-limit []
+  (get-in CONFIG-MAP [ENV :yipit-limit]))
+
+(defn config-folder []
+  (-> (.get (System/getenv) "INSTAFUN_HOME")
+      (str "/config")))
+
+(defn certificate-filename []
+  (str (config-folder) "/certificates/mfj_" (name (apns-env))  "_push.p12"))
+
+(defn setup-config []
+  (let [config-file (str (config-folder) "/insta.conf")
+        env (keyword (.get (System/getenv) "INSTAFUN_ENV"))]
+    (load-config config-file env)))
