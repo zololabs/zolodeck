34618606d9778951a5e2f2346f3a4a87945a549b
Siva Jagadeesan <sivajag@gmail.com>
2012-03-28 00:17:47 -0700

    Cleaned up project.clj Create mvn_repo with Datomic

--- api/project.clj
+++ api/project.clj
@@ -1,22 +1,18 @@
-(defproject zolodeck "0.1.0-SNAPSHOT"
-  :description "Zolodeck"
-  :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
-                 "local" ~(str (.toURI (java.io.File. 
-                                 (str (System/getProperty "user.name") "/.zolo.mvn"))))}
-  :dev-resources-path "script"
+(defproject com.currylogic/zolodeck-api "0.1.0-SNAPSHOT"
+  :description "Zolodeck API"
+
   :dependencies [[org.clojure/clojure "1.4.0-beta3"]
-;                 [org.clojure/clojure-contrib "1.2.0"]
-                 [compojure "0.6.2"]
-                 [ring/ring-core "0.3.7"]
-                 [ring/ring-jetty-adapter "0.2.3"]
-                 [sandbar/sandbar "0.3.0"]
+                 [compojure "1.0.0-RC2"]
+                 [ring "1.0.1"]
+                 [sandbar/sandbar "0.4.0-SNAPSHOT"]
+                 [clj-facebook-graph "0.4.0"]
                  [clj-base64 "0.0.1"]
                  [org.bituf/clj-stringtemplate "0.2"]
                  [org.slf4j/slf4j-log4j12 "1.6.4"]
-                 [clj-facebook-graph "0.2.0"]
                  [bouncycastle/bcprov-jdk16-nosign "140"]
                  [clj-http "0.1.3"]
                  [joda-time "1.6"]
+                 [slingshot "0.10.2"]
 
                  [org.clojars.nakkaya.javax.mail/imap "1.4.3"]
                  [org.clojars.nakkaya.javax.mail/mail "1.4.3"]
@@ -36,17 +32,34 @@
                  [backtype/jzmq "2.1.0"]
                  [com.googlecode.json-simple/json-simple "1.1"]
                  [com.googlecode/kryo "1.04"]
-                 [compojure "0.6.4"]
                  [hiccup "0.3.6"]
-                 [ring/ring-jetty-adapter "0.3.11"]
                  [org.slf4j/slf4j-log4j12 "1.5.8"]
                  [storm/carbonite "1.0.0"]
                  [org.yaml/snakeyaml "1.9"]
                  [org.apache.httpcomponents/httpclient "4.1.1"]
 
-                 [datomic/peer "0.1.2678"]
+                 [com.datomic/datomic "0.1.2753"]]
 
-]
-  :plugins [[lein-swank "1.4.3"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :plugins [[lein-swank "1.4.3"]
+            [lein-pprint "1.0.0"]
+            [lein-checkouts "1.0.0"]]
+  
+  :profiles {:dev {:resource-paths ["script"]
+                   :dependencies [[clj-stacktrace "0.2.4"]
+                                  [swank-clojure "1.3.3"]
+                                  [lein-ring "0.5.2"]
+                                  [ring-serve "0.1.2"]]}}
+  
+  :min-lein-version "1.7.0"
+  :test-selectors {:default (fn [t] (not (or (:integration v) (:regression v))))
+                   :integration :integration
+                   :regression :regression
+                   :all (fn [t] true)}
+  
+  :project-init (require 'clojure.pprint)
+  :warn-on-reflection true
+  
+  :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
+                 "local" ~(str (.toURI (java.io.File. "../mvn_repo")))}
+  
   :resources-path "config")
