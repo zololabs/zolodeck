367b3b05b9123780127ad145c5614846c9872173
Siva Jagadeesan <sivajag@gmail.com>
2012-03-28 14:39:50 -0700

    Added storm as dependencies

--- api/project.clj
+++ api/project.clj
@@ -2,41 +2,15 @@
   :description "Zolodeck API"
 
   :dependencies [[org.clojure/clojure "1.4.0-beta3"]
-                 [compojure "1.0.0-RC2"]
+                 [compojure "1.0.1"]
                  [ring "1.0.1"]
                  [sandbar/sandbar "0.4.0-SNAPSHOT"]
                  [clj-facebook-graph "0.4.0"]
-                 [clj-base64 "0.0.1"]
-                 [org.bituf/clj-stringtemplate "0.2"]
-                 [org.slf4j/slf4j-log4j12 "1.6.4"]
-                 [bouncycastle/bcprov-jdk16-nosign "140"]
-                 [clj-http "0.1.3"]
                  [joda-time "1.6"]
                  [slingshot "0.10.2"]
 
                  [org.clojars.nakkaya.javax.mail/imap "1.4.3"]
                  [org.clojars.nakkaya.javax.mail/mail "1.4.3"]
-
-
-                 [org.codehaus.janino/janino "2.5.16"]
-                 [org.apache.lucene/lucene-core "3.3.0"]
-                 [org.hornetq/hornetq-core "2.2.2.Final"]
-                 [com.amazonaws/aws-java-sdk "1.3.0"]
-
-                 [commons-io "1.4"]
-                 [org.apache.commons/commons-exec "1.1"]
-                 [storm/libthrift7 "0.7.0"]
-                 [clj-time "0.3.0"]
-                 [log4j/log4j "1.2.16"]
-                 [com.netflix.curator/curator-framework "1.0.1"]
-                 [backtype/jzmq "2.1.0"]
-                 [com.googlecode.json-simple/json-simple "1.1"]
-                 [com.googlecode/kryo "1.04"]
-                 [hiccup "0.3.6"]
-                 [org.slf4j/slf4j-log4j12 "1.5.8"]
-                 [storm/carbonite "1.0.0"]
-                 [org.yaml/snakeyaml "1.9"]
-                 [org.apache.httpcomponents/httpclient "4.1.1"]
 
                  [com.datomic/datomic "0.1.2753"]]
 
@@ -44,8 +18,13 @@
             [lein-pprint "1.1.0"]
             [lein-ring "0.6.2"]]
   
-  :profiles {:dev {:dependencies [[clj-stacktrace "0.2.4"]
-                                  [swank-clojure "1.3.3"]]}}
+  ;; :profiles {:dev {:dependencies [[clj-stacktrace "0.2.4"]
+  ;;                                 [swank-clojure "1.3.3"]
+  ;;                                 [storm "0.7.0"]]}}
+
+  :dev-dependencies [[clj-stacktrace "0.2.4"]
+                     [swank-clojure "1.3.3"]
+                     [storm "0.7.0"]]
   
   :min-lein-version "1.7.0"
   :test-selectors {:default (fn [t] (not (:integration t)))
