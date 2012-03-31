4107345f00144206d5246cb3622a2103ebba16b6
Amit Rathore <amitrathore@gmail.com>
2012-03-31 16:50:20 -0700

    work in progress around datomic schema

--- api/tools/repl.sh
+++ api/tools/repl.sh
@@ -2,7 +2,7 @@ CURRENT_DIR=$(cd `dirname $0`; pwd)
 INFERRED_ROOT=$(cd `dirname $0`; cd .. ; pwd)
 
 export ZOLODECK_ENV=development
-export ZOLODECK_HOME=${INSTAFUN_HOME:-"${CURRENT_DIR}"}
+export ZOLODECK_HOME=${INSTAFUN_HOME:-"${CURRENT_DIR}/../"}
 
 lein repl
 
