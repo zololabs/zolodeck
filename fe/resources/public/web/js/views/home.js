221b1b4a527827deaeeef69ed66a8378d52ccfe0
Siva Jagadeesan <sivajag@gmail.com>
2012-03-31 23:51:17 -0700

    facebook login popup showing up

--- fe/resources/public/web/js/views/home.js
+++ fe/resources/public/web/js/views/home.js
@@ -1,5 +1,8 @@
 (function($) {
     window.HomeView = Backbone.View.extend({
+        events: {
+            'click #facebook_login': 'login_using_facebook'
+        },
         
         initialize:function () {
             console.log('Initializing Home View');
@@ -9,6 +12,22 @@
         render:function (eventName) {
             $(this.el).html(this.template());
             return this;
+        },
+
+        login_using_facebook:function(){
+            opts = {
+                scope   : 'email,friends_about_me,friends_birthday,friends_relationship_details,friends_location,friends_likes,friends_website',
+                success : function (response) { console.log("You are authorized"); console.dir(response);},
+                error   : function (response) { console.log("You are not authorized : "); console.dir(response);}
+            };
+            
+            FB.login(function(response){
+                if (response.authResponse){
+                    opts.success(response);
+                }else{
+                    opts.error(response);
+                }
+            },{scope : opts.scope});
         }
         
     });
