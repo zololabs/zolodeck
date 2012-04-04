2a2d67ac56107fcb27c022b3b235bdcc0a81beb3
Siva Jagadeesan <sivajag@gmail.com>
2012-04-03 20:46:06 -0700

    added some db utility functions

---
+++ api/src/zolo/ext/facebook.clj
@@ -0,0 +1,23 @@
+(ns zolo.ext.facebook
+  (:use zolo.utils.debug)
+  (:require [clj-facebook-graph.auth :as fb-auth]
+            [clj-facebook-graph.client :as fb-client]))
+
+
+(def APP-ID "361942873847116")
+(def APP-SECRET "6a968bdeb2eb92ac913ec1b88f88cef6")
+
+(def facebook-oauth2
+  {:authorization-uri "https://graph.facebook.com/oauth/authorize"
+   :access-token-uri "https://graph.facebook.com/oauth/access_token"
+   :client-id APP-ID
+   :client-secret APP-SECRET
+   :access-query-param :access_token
+   :grant-type 'authorization-code})
+
+(defn decode-signed-request [encoded-signed-request] 
+  (fb-auth/decode-signed-request encoded-signed-request APP-SECRET))
+
+(defn code->token [code]
+  (fb-auth/get-access-token facebook-oauth2 {:code code}))
+
