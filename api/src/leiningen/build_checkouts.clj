6dc7f93c2221f0a55bcd431db3e320c1869d8ec5
Siva Jagadeesan <sivajag@gmail.com>
2012-04-06 17:08:48 -0700

    created a task to build checkout projects

---
+++ api/src/leiningen/build_checkouts.clj
@@ -0,0 +1,33 @@
+(ns leiningen.build-checkouts
+  (:use robert.hooke
+        leiningen.clean
+        clojure.pprint
+        [leiningen.core :only (apply-task read-project task-not-found)])
+  (:require [clojure.java.shell :as shell])
+  (:import [java.io File]))
+
+(defn error [msg-format & params]
+  (println (apply format (str "ERROR: " msg-format) params))
+  (System/exit 1))
+
+(defn user-dir []
+  (-> "user.dir" System/getProperty File.))
+
+(defn list-checkouts [^File dir]
+  (seq (.listFiles (File. dir "checkouts"))))
+
+(defn create-project [^File dir]
+  (read-project (.getAbsolutePath (File. dir "project.clj"))))
+
+(defn run-lein-task [task-name project]
+  (println "Performing " task-name "on " (:name project))
+  (apply-task task-name project [] task-not-found)
+  (println "Successfully " task-name " - " (:name project)))
+
+(defn build-checkouts [project & args]
+  (doseq [p (map create-project (list-checkouts (:dir project)))]
+    (run-lein-task "compile" p)))
+
+;; (add-hook #'leiningen.clean/clean
+;;           build-checkouts)
+
