2117d14b56a23d4cd32616b985fa5121d6b9c78b
Amit Rathore <amitrathore@gmail.com>
2012-06-06 12:16:35 -0700

    using the right app-id and app-secret to get all-users from the test-fb-app

--- api/test/zolo/utils/test_utils.clj
+++ api/test/zolo/utils/test_utils.clj
@@ -1,6 +1,8 @@
 (ns zolo.utils.test-utils
-  (:require [zolodeck.clj-social-lab.facebook.api :as api]))
+  (:require [zolodeck.clj-social-lab.facebook.api :as api]
+            [zolo.setup.config :as conf]))
 
 (defn hobbes-access-token []
-  (let [all (api/all api/APP-ID api/APP-ACCESS-TOKEN)]
+  (let [app-access-token (api/app-access-token (conf/app-id) (conf/app-secret))
+        all (api/all (conf/app-id) app-access-token)]
     (:access-token (first (filter #(= "100003858741258" (:id %)) all)))))
