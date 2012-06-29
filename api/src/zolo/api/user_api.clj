467584add28ca1eb03a451ad9d4759298f480bde
Siva Jagadeesan <sivajag@gmail.com>
2012-06-29 16:25:17 -0700

    always load user info from facebook

--- api/src/zolo/api/user_api.clj
+++ api/src/zolo/api/user_api.clj
@@ -17,13 +17,14 @@
 (defn fully-loaded-user
   ([user]
      (let [u-fb-id (:user/fb-id user)]
-       (if (= 0 (count (:user/contacts user)))
+       ;; (if (= 0 (count (:user/contacts user)))
          (do
            (user/update-facebook-friends u-fb-id)
            (user/update-facebook-inbox u-fb-id)
            (user/update-scores (user/reload user))
            (user/reload user))
-         user)))
+         ;;  user)
+         ))
   ([]
      (fully-loaded-user (sandbar/current-user))))
 
