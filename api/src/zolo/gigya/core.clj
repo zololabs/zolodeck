e8597f184d70cb14d268172c6431d1727339b441
Siva Jagadeesan <sivajag@gmail.com>
2012-09-10 13:49:11 -0700

    getting access-token only once

--- api/src/zolo/gigya/core.clj
+++ api/src/zolo/gigya/core.clj
@@ -16,30 +16,28 @@
 (defn gigya-oauth-headers [access-token]
   {"Authorization"(str "OAuth " access-token)})
 
-(defn access-token []
+(defn get-gigya-access-token []
   (-> (http-client/post "https://socialize.gigya.com/socialize.getToken" 
                         {:headers (gigya-basic-headers) 
                          :content-type "application/x-www-form-urlencoded"
                          :form-params {"grant_type" "none"}
                          :as :json})
       :body
-      :access_token
-      (print-vals-> "Access Token :")))
+      :access_token))
 
+(def gigya-access-token (get-gigya-access-token))
 
 (defn notify-registration [user gigya-user]
   (http-client/post "https://socialize.gigya.com/socialize.notifyRegistration" 
-                    {:headers (gigya-oauth-headers (access-token)) 
-                     :debug true
+                    {:headers (gigya-oauth-headers gigya-access-token) 
                      :form-params {"siteUID" (str (:user/guid user))
                                    "UID" (:UID gigya-user)
                                    "format" "json"}})
   user)
 
 (defn delete-account [site-uid]
-    (-> (http-client/post "https://socialize.gigya.com/socialize.deleteAccount" 
-                        {:headers (gigya-oauth-headers (access-token)) 
-                         :debug true
+  (-> (http-client/post "https://socialize.gigya.com/socialize.deleteAccount" 
+                        {:headers (gigya-oauth-headers gigya-access-token) 
                          :form-params {"UID" site-uid
                                        "format" "json"}})
       (print-vals-> "Response from Delete Account:")))
