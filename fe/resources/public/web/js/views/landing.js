dd54720dda1447579d1589680560052f6bddb311
Siva Jagadeesan <sivajag@gmail.com>
2012-09-26 20:33:45 -0700

    moved facebook code to its own view

--- fe/resources/public/web/js/views/landing.js
+++ fe/resources/public/web/js/views/landing.js
@@ -1,9 +1,10 @@
 define(['jquery',
         'underscore',
         'backbone',
+        'views/social/facebook_login',
         'text!templates/landing.html'],
 
-      function($, _, Backbone, landingTemplate){
+      function($, _, Backbone, FacebookLoginView, landingTemplate){
         
         var LandingView = Backbone.View.extend({
           
@@ -14,7 +15,7 @@ define(['jquery',
           },
 
           initialize:function () {
-            _.bindAll(this, 'render', 'renderFacebookLogin', 'loginUsingFacebook');
+            _.bindAll(this, 'render', 'loginUsingFacebook');
             
             this.user = this.model;
           },
@@ -25,44 +26,11 @@ define(['jquery',
             var compiledTemplate = _.template( landingTemplate, data );
 
             this.$el.html(compiledTemplate);
-
-            this.renderFacebookLogin();
+            
+            var facebookLoginView = new FacebookLoginView({model: this.user});
+            facebookLoginView.render();
 
             return this;
-          },
-
-          renderFacebookLogin: function(){
-            console.log("Rendering Facebook Login");
-
-            var user = this.user;
-
-            window.fbAsyncInit = function() {
-              FB.init({
-                appId      : '361942873847116',
-                //channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel File
-                status     : true, // check login status
-                cookie     : true, // enable cookies to allow the server to access the session
-                xfbml      : true  // parse XFBML
-              });
-               
-              FB.Event.subscribe('auth.statusChange', function(response) {
-                if (response && ("connected" == response.status)){
-                  user.login("FACEBOOK");
-                } else {
-                  user.logout();
-                }
-              });
-            };
-            
-            // Load the SDK Asynchronously
-            (function(d){
-              var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-              if (d.getElementById(id)) {return;}
-              js = d.createElement('script'); js.id = id; js.async = true;
-              js.src = "//connect.facebook.net/en_US/all.js";
-              ref.parentNode.insertBefore(js, ref);
-            }(document));
-            
           },
 
           loginUsingFacebook: function(){
