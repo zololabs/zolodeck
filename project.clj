4776124235794900a08f520cccf23b34caa9a11f
Amit Rathore <amitrathore@gmail.com>
2012-10-08 11:41:12 -0700

    added storm to project.clj

--- project.clj
+++ project.clj
@@ -39,7 +39,8 @@
 
   :hooks [leiningen.hooks.difftest]
 
-  :dev-dependencies [[clj-stacktrace "0.2.4"]
+  :dev-dependencies [[storm "0.8.1"]
+                     [clj-stacktrace "0.2.4"]
                      [swank-clojure "1.3.3"]
                      [ring-serve "0.1.2"]
                      [zolodeck/clj-social-lab "1.0.0-SNAPSHOT"]
