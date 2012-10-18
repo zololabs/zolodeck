5bf449e1364a5f8e17e81f6485a903bbc1941a9a
Amit Rathore <amitrathore@gmail.com>
2012-10-18 15:07:00 -0700

    improved logger macros to work with threads

--- src/zolo/utils/logger.clj
+++ src/zolo/utils/logger.clj
@@ -14,30 +14,77 @@
   (let [s (with-out-str (pprint things))]
     (snip s)))
 
-(defmacro trace
-  "Trace level logging using print-style args."
-  {:arglists '([message & more] [throwable message & more])}
-  [& args]
-  `(logger/trace (snipped-pretty ~@args)))
+;; (defmacro trace
+;;   "Trace level logging using print-style args."
+;;   {:arglists '([message & more] [throwable message & more])}
+;;   [& args]
+;;   `(logger/trace (snipped-pretty ~@args)))
 
-(defmacro debug
-  "Debug level logging using print-style args."
-  {:arglists '([message & more] [throwable message & more])}
-  [& args]
-  `(logger/debug (snipped-pretty ~@args)))
+(defmacro trace [& args]
+  `(let [l# (list ~@args)
+         v# (apply snipped-pretty l#)]
+     (logger/trace v#)
+     (last l#)))
 
+(defmacro trace-> [& args]
+  `(let [l# (list ~@args)
+         v# (apply snipped-pretty l#)]
+     (logger/trace v#)
+     (first l#)))
 
-(defmacro info
-  "Info level logging using print-style args."
-  {:arglists '([message & more] [throwable message & more])}
-  [& args]
-  `(logger/info ~@args))
+(defmacro debug [& args]
+  `(let [l# (list ~@args)
+         v# (apply snipped-pretty l#)]
+     (logger/debug v#)
+     (last l#)))
 
-(defmacro warn
-  "Warn level logging using print-style args."
-  {:arglists '([message & more] [throwable message & more])}
-  [& args]
-  `(logger/warn (snipped-pretty ~@args)))
+(defmacro debug-> [& args]
+  `(let [l# (list ~@args)
+         v# (apply snipped-pretty l#)]
+     (logger/debug v#)
+     (first l#)))
+
+;; (defmacro debug
+;;   "Debug level logging using print-style args."
+;;   {:arglists '([message & more] [throwable message & more])}
+;;   [& args]
+;;   `(logger/debug (snipped-pretty ~@args)))
+
+(defmacro info [& args]
+  `(let [l# (list ~@args)
+         v# (apply snipped-pretty l#)]
+     (logger/info v#)
+     (last l#)))
+
+(defmacro info-> [& args]
+  `(let [l# (list ~@args)
+         v# (apply snipped-pretty l#)]
+     (logger/info v#)
+     (first l#)))
+
+;; (defmacro info
+;;   "Info level logging using print-style args."
+;;   {:arglists '([message & more] [throwable message & more])}
+;;   [& args]
+;;   `(logger/info ~@args))
+
+(defmacro warn [& args]
+  `(let [l# (list ~@args)
+         v# (apply snipped-pretty l#)]
+     (logger/warn v#)
+     (last l#)))
+
+(defmacro warn-> [& args]
+  `(let [l# (list ~@args)
+         v# (apply snipped-pretty l#)]
+     (logger/warn v#)
+     (first l#)))
+
+;; (defmacro warn
+;;   "Warn level logging using print-style args."
+;;   {:arglists '([message & more] [throwable message & more])}
+;;   [& args]
+;;   `(logger/warn (snipped-pretty ~@args)))
 
 (defmacro error
   "Error level logging using print-style args."
