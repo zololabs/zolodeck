3d4cb5796bb2496b730a306f8487e9e283119224
Amit Rathore <amitrathore@gmail.com>
2013-01-11 15:47:23 -0800

    added fn in server_api to check datomic status, need to call it from the web-app/routes

--- src/zolo/api/server_api.clj
+++ src/zolo/api/server_api.clj
@@ -2,7 +2,46 @@
   (:use zolodeck.utils.debug)
   (:require
    [zolo.domain.user :as user]
+   [zolodeck.utils.calendar :as zolo-cal]
+   [zolodeck.demonic.core :as demonic]
    [zolo.utils.logger :as logger]))
+
+(def SERVER_GUID #uuid "97a34244-61fe-4a05-8a9e-95013349e298")
+
+(defn- new-server-obj []
+  (-> {:server/guid SERVER_GUID :server/last-updated (zolo-cal/now-instant)}
+      demonic/insert))
+
+(defn- find-server-object []
+  (-> (demonic/run-query '[:find ?u :in $ ?guid :where [?u :server/guid ?guid]] SERVER_GUID)
+      ffirst
+      demonic/load-entity))
+
+(defn- find-or-create-server-object []
+  (let [obj (find-server-object)]
+    (if obj obj (new-server-obj))))
+
+(defn- within-minutes [old-inst new-inst number-of-minutes]
+  (if-not (and old-inst new-inst)
+    false
+    (<= (zolo-cal/minutes-between old-inst new-inst) number-of-minutes)))
+
+(defn- update-server-obj [server-obj updated-time]
+  (-> server-obj
+      (assoc :server/last-updated updated-time)
+      demonic/insert))
+
+(defn check-datomic []
+  (let [now (zolo-cal/now-instant)
+        server-obj (find-or-create-server-object)
+        last-updated (:server/last-updated server-obj)]
+    (if (within-minutes last-updated now 7)
+      (do
+        (update-server-obj server-obj now)
+        true)
+      (do
+        (update-server-obj server-obj now)
+        false))))
 
 (defn status [request-params]
   {:no-of-users (user/count-users)})
