08000dd63ab82bb37b159f0953ed3c14cf088d93
Siva Jagadeesan <sivajag@gmail.com>
2013-01-23 16:35:39 -0800

    fetch contacts in social layer tested

--- test/zolo/social/facebook/core_test.clj
+++ test/zolo/social/facebook/core_test.clj
@@ -14,13 +14,40 @@
       lab/login-creds
       (assoc :provider "FACEBOOK")))
 
+(defn fake-friends-list [at uid]
+  (-> uid
+      lab/get-user
+      lab/fetch-friends))
+
+(defn fake-extended-user-info [at uid]
+  (-> uid
+      lab/get-user
+      lab/extended-user-info))
+
 (deftest test-signup-user
   (lab/in-facebook-lab
-   (stubbing [fb-gateway/extended-user-info lab/extended-user-info]
+   (stubbing [fb-gateway/extended-user-info fake-extended-user-info]
      (let [mickey (lab/create-user "Mickey" "Mouse")
            params (request-params mickey)
            cookies {}
            canonical-user (social/signup-user params cookies)]
        (assert-basic-user-info mickey canonical-user)
        (assert-user-identity mickey (first (:user/user-identities canonical-user)))))))
+
+
+(deftest test-fetch-contacts
+  (lab/in-facebook-lab
+   (stubbing [fb-gateway/friends-list fake-friends-list]
+     (let [mickey (lab/create-user "Mickey" "Mouse")
+           donald (lab/create-friend "Donald" "Duck")
+           daisy (lab/create-friend "Daisy" "Duck")]
+       (lab/make-friend mickey donald)
+       (lab/make-friend mickey daisy)
+
+       (let [contacts (sort-by :contact/first-name (social/fetch-contacts :provider/facebook (:access-token mickey) (:uid mickey) nil))
+             daisy-contact (first contacts)
+             donald-contact (last contacts)]
+         (is (= 2 (count contacts)))
+         (assert-contact daisy daisy-contact)
+         (assert-contact donald donald-contact))))))
 
--- test/zolo/test/assertions.clj
+++ test/zolo/test/assertions.clj
@@ -5,10 +5,11 @@
   (:require [zolo.social.core :as social]))
 
 (defn assert-map-values [m1 m1-keys m2 m2-keys]
-  (is (= (count (remove nil? (vals (select-keys m1 m1-keys))))
-           (count (remove nil? (vals (select-keys m2 m2-keys))))
-           (count m1-keys)
-           (count m2-keys)) "No of keys don't match")
+  (is (= (count m1-keys) (count m2-keys)) "No of keys don't match")
+
+  (doall (map #(is (not (nil? (m1 %))) (str % " shouldn't be nil in m1")) m1-keys))
+  (doall (map #(is (not (nil? (m2 %))) (str % " shouldn't be nil in m2")) m2-keys))
+
   (doall (map #(is (= (%1 m1) (%2 m2)) (str %1 " does not match " %2)) m1-keys m2-keys)))
 
 (defn assert-basic-user-info [fb-user canonical-user]
@@ -20,10 +21,28 @@
   (let [fb-user-keys [:uid :first_name :last_name :username :locale :email :pic_small :pic_big :profile_url]
         canonical-user-identity-keys [:identity/provider-uid :identity/first-name :identity/last-name :identity/nickname :identity/locale  :identity/email  :identity/thumbnail-url :identity/photo-url :identity/profile-url ]]
     (assert-map-values fb-user fb-user-keys canonical-user-identity canonical-user-identity-keys)
-    ;; assert birthday
+    (is (= (:birthday_date fb-user) (str (:identity/birth-month canonical-user-identity) "/"
+                                         (:identity/birth-day canonical-user-identity) "/"
+                                         (:identity/birth-year canonical-user-identity))))
     (is (= (social/gender-enum (:sex fb-user)) (:identity/gender canonical-user-identity)))
-    ;; assert location
-    ))
+    (assert-map-values (:current_location fb-user) [:country :state :city :zip]
+                       canonical-user-identity [:identity/country :identity/state :identity/city :identity/zip])))
+
+(defn assert-social-identity [fb-contact canonical-social-identity]
+  (let [fb-contact-keys [:last_name :first_name :link :username :id]
+        canonical-si-keys [:social/last-name :social/first-name :social/profile-url :social/nickname :social/provider-uid]]
+    (assert-map-values fb-contact fb-contact-keys canonical-social-identity canonical-si-keys)
+    (is (= (:birthday fb-contact) (str (:social/birth-month canonical-social-identity) "/"
+                                       (:social/birth-day canonical-social-identity) "/"
+                                       (:social/birth-year canonical-social-identity))))
+
+    (is (= (social/gender-enum (:gender fb-contact)) (:social/gender canonical-social-identity)))))
+
+(defn assert-contact [fb-contact canonical-contact]
+  (let [fb-contact-keys [:first_name :last_name]
+        canonical-contact-keys [:contact/first-name :contact/last-name]]
+    (assert-map-values fb-contact fb-contact-keys canonical-contact canonical-contact-keys)
+    (assert-social-identity fb-contact (first (:contact/social-identities canonical-contact)))))
 
 ;; Domain Related Ones
 (defn assert-contacts-are-same [expected-contact actual-contact]
