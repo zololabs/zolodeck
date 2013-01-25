e2b7a525bc5fc162b596a97eaa24ae31a5cded57
Siva Jagadeesan <sivajag@gmail.com>
2013-01-25 13:49:19 -0800

    asserting contacts and si

--- test/zolo/domain/contact_test.clj
+++ test/zolo/domain/contact_test.clj
@@ -10,6 +10,7 @@
             [zolo.domain.user :as user]
             [zolo.social.core :as social]
             [zolo.test.assertions.datomic :as db-assert]
+            [zolo.test.assertions.domain :as d-assert]
             [zolo.domain.contact :as contact]
             [zolo.setup.datomic-setup :as datomic-setup]
             [zolodeck.clj-social-lab.facebook.core :as fb-lab]))
@@ -25,6 +26,7 @@
      (let [mickey (fb-lab/create-user "Mickey" "Mouse")
            donald (fb-lab/create-friend "Donald" "Duck")
            daisy (fb-lab/create-friend "Daisy" "Duck")
+           minnie (fb-lab/create-friend "Minnie" "Mouse")
            db-mickey (in-demarcation (user/signup-new-user (create-social-user mickey)))]
 
        (fb-lab/make-friend mickey donald)
@@ -42,12 +44,13 @@
         (db-assert/assert-datomic-social-count 2))
 
        (in-demarcation
-        (fb-lab/make-friend mickey (fb-lab/create-friend "Minnie" "Mouse"))
+        (fb-lab/make-friend mickey minnie)
         (contact/update-contacts (user/reload db-mickey))
         
         (db-assert/assert-datomic-contact-count 3)
         (db-assert/assert-datomic-social-count 3))
 
-
-       ;; Assert Values
-       ))))
+       (let [[db-daisy db-donald db-minnie] (sort-by :contact/first-name (:user/contacts (in-demarcation (user/reload db-mickey))))]
+         (d-assert/contacts-are-same daisy db-daisy)
+         (d-assert/contacts-are-same donald db-donald)
+         (d-assert/contacts-are-same minnie db-minnie))))))
---
+++ test/zolo/test/assertions/domain.clj
@@ -0,0 +1,24 @@
+(ns zolo.test.assertions.domain
+  (:use zolodeck.utils.debug
+        zolodeck.utils.clojure
+        zolo.test.assertions.core
+        [clojure.test :only [is are]])
+  (:require [zolo.social.core :as social]
+            [zolo.social.facebook.messages :as fb-messages]
+            [zolodeck.utils.calendar :as zolo-cal]
+            [zolodeck.demonic.core :as demonic]))
+
+(defn social-identities-are-same [fb-contact db-si]
+  (assert-map-values fb-contact [:username :link :id :last_name :first_name]
+                     db-si [:social/nickname :social/profile-url :social/provider-uid :social/last-name :social/first-name])
+
+  (is (= (:birthday fb-contact) (str (:social/birth-month db-si) "/"
+                                     (:social/birth-day db-si) "/"
+                                     (:social/birth-year db-si))))
+  (is (= (social/gender-enum (:gender fb-contact)) (:social/gender db-si))))
+
+(defn contacts-are-same [fb-contact db-contact]
+  (assert-map-values fb-contact [:first_name :last_name]
+                     db-contact [:contact/first-name :contact/last-name])
+
+  (social-identities-are-same fb-contact (first (:contact/social-identities db-contact))))
