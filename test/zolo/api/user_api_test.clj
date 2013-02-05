ea30ac123464202944192054cdd760a601f40a4c
Siva Jagadeesan <sivajag@gmail.com>
2013-02-05 11:25:59 -0800

    Uncommented Tests

--- test/zolo/api/user_api_test.clj
+++ test/zolo/api/user_api_test.clj
@@ -18,11 +18,9 @@
         was-request-successful?
         assert-user-present-in-datomic))
 
-  '(demonic-testing "New user with invalid facebook login"
+  (demonic-testing "New user with invalid facebook login"
     (-> (new-scenario)
         assert-user-not-present-in-datomic
         post-new-user
         (was-response-status? (:forbidden STATUS-CODES))
-        assert-user-not-present-in-datomic))
-
-  )
+        assert-user-not-present-in-datomic)))
