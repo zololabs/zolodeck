b39a41cb38e0f674e689db5108e12440205334fa
Amit Rathore <amitrathore@gmail.com>
2013-02-13 12:47:42 -0800

    added integration tests for fetch-user-info, fetch-friends, fetch-inbox, fetch-feed

--- test/zolo/social/facebook/fb_api_test.clj
+++ test/zolo/social/facebook/fb_api_test.clj
@@ -5,9 +5,14 @@
         conjure.core)
     (:require [zolodeck.clj-social-lab.facebook.core :as fb-lab]
               [zolo.social.facebook.gateway :as fb-gateway]
+              [zolo.social.facebook.messages :as fb-messages]
+              [zolo.social.facebook.stream :as fb-stream]              
+              [zolodeck.utils.calendar :as zolo-cal]
               [zolo.setup.config :as conf]))
 
 (def JACK-FB-ID "100005253966931")
+
+(def START-TIMESTAMP (-> #inst "2000-10-22" .getTime zolo-cal/to-seconds))
 
 (defn jack-access-token []
   (let [app-access-token (fb-gateway/app-access-token (conf/fb-app-id) (conf/fb-app-secret))
@@ -36,6 +41,49 @@
          (is (same-value? (keys (get-in (first jack-friends-list) [:picture :data]))
                           (keys (get-in mf [:picture :data])))))))))
 
-;; test-fetch-inbox
+(deftest ^:integration test-fetch-inbox
+  (fb-lab/in-facebook-lab
+      (let [mickey (fb-lab/create-user "Mickey" "Mouse")
+            donald (fb-lab/create-user "Donald" "Duck")
+            daisy (fb-lab/create-user "Daisy" "Duck")]
+        
+        (fb-lab/login-as mickey)
+        
+        (fb-lab/make-friend mickey donald)
+        (fb-lab/make-friend mickey daisy)
 
-;; test-recent-activity
+        (fb-lab/send-message mickey donald "1" "Hi, what's going on?" "2012-05-01")
+        (fb-lab/send-message donald mickey "1" "Nothing, just work..." "2012-05-02")
+        (fb-lab/send-message mickey donald "1" "OK, should I get groceries?" "2012-05-03")
+        (fb-lab/send-message mickey daisy "2" "Hi, how's  it going?" "2012-06-01")
+        (fb-lab/send-message daisy mickey "2" "Good, I finished writing the tests" "2012-06-02")
+
+        (let [jack-messages (fb-messages/fetch-inbox (jack-access-token) START-TIMESTAMP)
+              mickey-messages (fb-lab/fetch-messages mickey)]
+          ;; TODO - test for subject when support is added to Zolodeck
+          ;; TODO - test for attachment when support is added to Zolodeck
+          (doseq [mm mickey-messages]
+            (is (same-value? (remove #{:subject} (keys (first jack-messages)))
+                             (remove #{:attachment} (keys mm)))))))))
+
+(deftest ^:integration test-recent-activity
+  (fb-lab/in-facebook-lab
+   (let [mickey (fb-lab/create-user "Mickey" "Mouse")
+         donald (fb-lab/create-user "Donald" "Duck")
+         daisy (fb-lab/create-user "Daisy" "Duck")]
+     
+     (fb-lab/login-as mickey)
+     
+     (fb-lab/make-friend mickey donald)
+     (fb-lab/make-friend mickey daisy)
+
+     (fb-lab/post-to-wall mickey donald "hey check this link out" "2012-05-01")
+     (fb-lab/post-to-wall mickey daisy "happy birthday" "2012-05-02")
+     (fb-lab/post-to-wall daisy donald "check this picture" "2012-06-02")     
+
+     (let [jack-feed (print-vals "J:" (fb-stream/recent-activity (jack-access-token) JACK-FB-ID START-TIMESTAMP))
+           mickey-feed (print-vals "M:" (fb-lab/fetch-feeds donald))]
+       (doseq [mf mickey-feed]
+         (doseq [jfk (keys (first jack-feed))]
+           (is (some #{jfk} (keys mf)))))
+       ))))
