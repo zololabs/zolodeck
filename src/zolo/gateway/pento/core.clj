61e0bae438707c917d79ce649107456dd3db8385
Amit Rathore <amitrathore@gmail.com>
2013-06-13 15:11:31 -0700

    updated pento/score to read json from pento service

--- src/zolo/gateway/pento/core.clj
+++ src/zolo/gateway/pento/core.clj
@@ -1,15 +1,16 @@
 (ns zolo.gateway.pento.core
+  (:use zolo.utils.debug)
   (:require [zolo.setup.config :as conf]
             [clj-http.client :as http]
-            [clojure.data.json :as json]
-            ))
+            [clojure.data.json :as json]))
 
 (defn classify-url [email-address]
-  (str "http://" (conf/pento-host) "?email=" email-address))
+  (print-vals (str "http://" (conf/pento-host) "/classify?email=" email-address)))
 
 (defn score [email-address]
-  (-> email-address
-      classify-url
-      http/get
-      json/read-json
-      :score))
+  (let [json-map (-> email-address
+                     classify-url
+                     http/get
+                     :body
+                     json/read-json)]
+    (-> email-address keyword json-map float)))
