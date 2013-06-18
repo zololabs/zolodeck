ef34f7446a98882fe9523939d798c0dcce903d9a
Amit Rathore <amitrathore@gmail.com>
2013-06-17 17:11:06 -0700

    data-ready-in should be >= 90 minutes (not just > 90 mins)

--- test/zolo/service/user_service_test.clj
+++ test/zolo/service/user_service_test.clj
@@ -122,7 +122,7 @@
              d-mickey (u-store/reload distilled-mickey)]
          (is (= ["Mickey.Mouse@gmail.com"] (:user/emails distilled-mickey)))
          (is (:user/data-ready-in distilled-mickey))
-         (is (> (:user/data-ready-in distilled-mickey) (* 90 60)))         
+         (is (>= (:user/data-ready-in distilled-mickey) (* 90 60)))         
 
          (is (-> d-mickey :user/user-identities first :identity/permissions-granted))
          (db-assert/assert-datomic-user-count 1)
