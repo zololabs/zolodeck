c11cc1414587618d428dde2a8630e45bf8965560
Amit Rathore <amitrathore@gmail.com>
2013-07-18 16:56:08 -0700

    refactoring stats-service to use let-user-entity macro

--- src/zolo/service/stats_service.clj
+++ src/zolo/service/stats_service.clj
@@ -11,29 +11,30 @@
             [zolo.service.distiller.contact :as c-distiller]))
 
 
-(defn contact-stats [u]
-  (when u
+(defn contact-stats [user-guid]
+  (service/let-user-entity [u user-guid]
     (d-core/run-in-tz-offset (:user/login-tz u)
-                             (let [contacts (sort-by contact/contact-score (contact/person-contacts u))
-                                   ibc (interaction/ibc u contacts)]
-                               {:total (count contacts)
-                                :strong (count (contact/strong-contacts u))
-                                :medium (count (contact/medium-contacts u))
-                                :weak   (count (contact/weak-contacts u))
-                                :quartered (count (contact/contacts-not-contacted-for-days ibc 90))
-                                :strongest-contact (c-distiller/distill (last contacts) u ibc)
-                                :weakest-contact (c-distiller/distill (first contacts) u ibc)}))))
+      (let [contacts (sort-by contact/contact-score (contact/person-contacts u))
+            ibc (interaction/ibc u contacts)]
+        {:total (count contacts)
+         :strong (count (contact/strong-contacts u))
+         :medium (count (contact/medium-contacts u))
+         :weak   (count (contact/weak-contacts u))
+         :quartered (count (contact/contacts-not-contacted-for-days ibc 90))
+         :strongest-contact (c-distiller/distill (last contacts) u ibc)
+         :weakest-contact (c-distiller/distill (first contacts) u ibc)}))))
 
-(defn interaction-stats [u]
-  (d-core/run-in-tz-offset (:user/login-tz u)
-                           (let [contacts (sort-by contact/contact-score (contact/person-contacts u))
-                                 ibc (interaction/ibc u contacts)
-                                 interactions (interaction/interactions-from-ibc ibc)]
-                             {:best-week-interaction-count
-                              (s-int-dist/best-week-interaction-count interactions)
-                              :weekly-average
-                              (s-int-dist/weekly-average interactions)
-                              :all-week-interaction-count
-                              (interaction/all-interaction-count-in-the-past ibc 7)
-                              :interaction-daily-counts
-                              (interaction/daily-counts-for-network ibc)})))
+(defn interaction-stats [user-guid]
+  (service/let-user-entity [u user-guid]
+    (d-core/run-in-tz-offset (:user/login-tz u)
+      (let [contacts (sort-by contact/contact-score (contact/person-contacts u))
+            ibc (interaction/ibc u contacts)
+            interactions (interaction/interactions-from-ibc ibc)]
+        {:best-week-interaction-count
+         (s-int-dist/best-week-interaction-count interactions)
+         :weekly-average
+         (s-int-dist/weekly-average interactions)
+         :all-week-interaction-count
+         (interaction/all-interaction-count-in-the-past ibc 7)
+         :interaction-daily-counts
+         (interaction/daily-counts-for-network ibc)}))))
