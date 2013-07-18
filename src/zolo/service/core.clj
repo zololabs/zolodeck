5bf6e979544b621c03a4a3e4445ccc7970de357f
Amit Rathore <amitrathore@gmail.com>
2013-07-18 16:48:29 -0700

    refactored to add let-user, let-user-entity, and let-message-entity macros, that can be used in services, and they log the fact when their subject isn't found

--- src/zolo/service/core.clj
+++ src/zolo/service/core.clj
@@ -14,3 +14,22 @@
 (defn provider-string->provider-enum [provider-string]
   (or (social/provider-enum (.toUpperCase provider-string))
       (throw (RuntimeException. (str "Unknown provider string specified: " provider-string)))))
+
+(defn missing-message-error [ui-guid m-id]
+  (str "Message not found for UI-GUID, MESSAGE-ID: [" ui-guid "," m-id "]"))
+
+(defn missing-user-error [user-guid]
+  (str "User not found for USER-GUID: [" user-guid "]"))
+
+(defmacro let-user-entity [[user-sym user-guid] & body]
+  `(zolo.utils.clojure/unless-log [~user-sym (zolo.store.user-store/find-entity-by-guid ~user-guid)] (zolo.service.core/missing-user-error ~user-guid)
+                                  ~@body))
+
+(defmacro let-user [[user-sym user-guid] & body]
+  `(zolo.utils.clojure/unless-log [~user-sym (zolo.store.user-store/find-by-guid ~user-guid)] (zolo.service.core/missing-user-error ~user-guid)
+                                  ~@body))
+
+(defmacro let-message-entity [[message-sym ui-guid message-id] & body]
+  `(zolo.utils.clojure/unless-log
+    [~message-sym (zolo.store.message-store/find-by-ui-guid-and-id ~ui-guid ~message-id)] (zolo.service.core/missing-message-error ~ui-guid ~message-id)
+      ~@body))
