012e5603bae0d499e8fa412287b66175e9d5cd72
Amit Rathore <amitrathore@gmail.com>
2013-07-22 11:24:42 -0700

    added Message-ID to outgoing emails

--- src/zolo/social/email/utils.clj
+++ src/zolo/social/email/utils.clj
@@ -1,15 +1,19 @@
 (ns zolo.social.email.utils
-  (:use zolo.utils.debug)
+  (:use zolo.utils.debug
+        zolo.utils.clojure)
   (:require [clj-http.client :as http]))
 
+(defn random-string []
+  (.replaceAll (random-guid-str) "-" "."))
+
 (defn rfc822-message [from to reply-to-message-id subject body]
-  (-> (str "From: " from
-           "\r\nTo: " to
-           "\r\nSubject: " subject
-           "\r\nIn-Reply-To: " reply-to-message-id
-           "\r\nReferences:" reply-to-message-id
-           "\r\n\r\n" body)
-      print-vals))
+  (str "From: " from
+       "\r\nTo: " to
+       "\r\nSubject: " subject
+       "\r\nIn-Reply-To: " reply-to-message-id
+       "\r\nReferences:" reply-to-message-id
+       "\r\nMessage-ID: <" (random-string) ".mailer@api.zolodeck.com>"
+       "\r\n\r\n" body))
 
 (defn encoded-post-param [from subject message]
   (-> {:from from :subject subject :body message}
