41219b737518d89317e826def8ebd0c031241dda
Amit Rathore <amitrathore@gmail.com>
2013-08-16 15:04:47 -0700

    added prefixes to the metrics-name to reflect API server, and server-name, and process-name (hardcoded for now)

--- src/zolo/service/metrics_collector.clj
+++ src/zolo/service/metrics_collector.clj
@@ -7,26 +7,27 @@
             [zolo.setup.config :as conf]
             [zolo.utils.logger :as logger]))
 
+(defn metrics-name-for [mname]
+  (str "api.server0.p0." mname))
+
 (defmulti harvest-value (fn [metrics-name metrics-map] (:type metrics-map)))
 
 (defmethod harvest-value :meter [metrics-name metrics-map]
-  {:name metrics-name :value (get-in metrics-map [:rates 1])})
+  {:name (metrics-name-for metrics-name) :value (get-in metrics-map [:rates 1])})
 
 (defmethod harvest-value :timer [metrics-name metrics-map]
-  {:name metrics-name :value (get-in metrics-map [:rates 1])})
+  {:name (metrics-name-for metrics-name) :value (get-in metrics-map [:rates 1])})
 
 (defmethod harvest-value :counter [metrics-name metrics-map]
-  {:name metrics-name :value (:value metrics-map)})
+  {:name (metrics-name-for metrics-name) :value (:value metrics-map)})
 
 (defn collect-for-reporting [collector [metrics-name metrics-map]]
   (conj collector (harvest-value metrics-name metrics-map)))
 
 (defn report-all-metrics []
-  (logger/info "Gathering metrics to send out...")
   (it-> (metrics/all-metrics-clj)
         (reduce collect-for-reporting [] it)
-        (librato/collate (conf/librato-username) (conf/librato-key) it []))
-  (logger/info "... sent metrics!"))
+        (librato/collate (conf/librato-username) (conf/librato-key) it [])))
 
 (defn start []
   (zthread/run-thunk-periodically "LibratoReporter" report-all-metrics (* 30 1000)))
